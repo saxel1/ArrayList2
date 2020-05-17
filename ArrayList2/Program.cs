884ddcdc202504c7b1c7b3b0d2629b5a52c02aa0
Axel S <s.axel@gmx.de>
2020-05-17 15:56:52 +0200

    Projektdateien hinzufügen.

---
+++ ArrayList2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+
+public class Point
+{
+
+
+    public int x;
+    public int y;
+
+
+
+
+    public Point(int x, int y)
+    {
+        x = x;
+        y = y;
+    }
+    public string ToString()
+    {
+        return x + "," + y;
+    }
+}
+public class ArrayListTest
+{
+
+    public static void Main(string[] args)
+    {
+
+        ArrayList AL = new ArrayList();
+        Random R = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            Point p = new Point(R.Next(50), R.Next(50));
+            AL.Add(p);
+        }
+        PrintValues(AL);
+        AL.Sort();
+        PrintValues(AL);
+    }
+}
